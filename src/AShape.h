1e201fbc31c9f8a4dfe682c3d99b4e671e922c48
Yanyifan Liao <ben.jaminlyyf21@gmail.com>
2024-07-11 00:10:58 +0200

    Finish Assignment 1

---
+++ src/AShape.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+#include <memory>
+#include <iostream>
+
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/Shape.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+class AShape
+{
+public:
+    std::shared_ptr<sf::Shape> shape;
+    // name in the shape
+    sf::Text shape_name;
+    // speed of the shape
+    sf::Vector2f speed;
+
+    AShape(const sf::Vector2f pos, const sf::Vector2f speed,
+           const sf::Color color,
+           const sf::Vector2f size)
+    {
+        shape = std::make_shared<sf::RectangleShape>(size);
+        shape->setPosition(pos);
+        shape->setFillColor(color);
+        this->speed = speed;
+    }
+
+    AShape(const sf::Vector2f pos, const sf::Vector2f speed,
+           const sf::Color color,
+           const float radius)
+    {
+        shape = std::make_shared<sf::CircleShape>(radius);
+        shape->setPosition(pos);
+        shape->setFillColor(color);
+        this->speed = speed;
+    }
+
+    void setText(const std::string& name, const sf::Font& font, const sf::Color& color, const int font_size)
+    {
+        shape_name.setFont(font);
+        shape_name.setString(name);
+        shape_name.setFillColor(color);
+        shape_name.setCharacterSize(font_size);
+        shape_name.setOrigin(shape_name.getLocalBounds().left + shape_name.getLocalBounds().width * 0.5f,
+                             shape_name.getLocalBounds().top + shape_name.getLocalBounds().height * 0.5f);
+        shape_name.setPosition(
+            shape->getPosition().x + shape->getLocalBounds().width * 0.5f,
+            shape->getPosition().y + shape->getLocalBounds().height * 0.5f);
+    }
+
+    void move()
+    {
+        shape->move(speed);
+        shape_name.move(speed);
+    }
+
+    void draw(sf::RenderWindow* window) const
+    {
+        window->draw(*shape);
+        window->draw(shape_name);
+    }
+};
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,24 +1,103 @@
+#include <fstream>
+#include <iostream>
 #include <SFML/Graphics.hpp>
+
+#include "AShape.h"
+
+std::vector<std::shared_ptr<AShape>> shapes;
+// window config
+unsigned int window_w, window_h;
+sf::Font font;
+
+void readConfigFile(const std::string& file_name)
+{
+    std::ifstream fin(file_name);
+    std::string mode;
+
+    // font configs
+    std::string font_path;
+    sf::Color font_color;
+    int font_size = 30;
+
+
+    // shape configs
+    std::string name;
+    float px, py, sx, sy, w, h, radius;
+
+    // color configs
+    int r, g, b;
+
+    while (fin >> mode)
+    {
+        if (mode == "Window")
+        {
+            fin >> window_w >> window_h;
+        }
+        else if (mode == "Font")
+        {
+            fin >> font_path >> font_size >> r >> g >> b;
+            if (!font.loadFromFile(font_path))
+            {
+                std::cerr << "Could not load the font!" << '\n';
+                return;
+            }
+            font_color = sf::Color(r, g, b);
+        }
+        else if (mode == "Circle")
+        {
+            fin >> name >> px >> py >> sx >> sy >> r >> g >> b >> radius;
+            AShape as(sf::Vector2f(px, py), sf::Vector2f(sx, sy),
+                      sf::Color(r, g, b), radius);
+            as.setText(name, font, font_color, font_size);
+            shapes.push_back(std::make_shared<AShape>(as));
+        }
+        else if (mode == "Rectangle")
+        {
+            fin >> name >> px >> py >> sx >> sy >> r >> g >> b >> w >> h;
+            AShape as(sf::Vector2f(px, py), sf::Vector2f(sx, sy),
+                      sf::Color(r, g, b), sf::Vector2f(w, h));
+            as.setText(name, font, font_color, font_size);
+            shapes.push_back(std::make_shared<AShape>(as));
+        }
+    }
+}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML Test");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+    readConfigFile("config.txt");
+    sf::RenderWindow window(sf::VideoMode(window_w, window_h), "SFML Test");
+    // set this to avoid speed too fast
+    // window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+        window.clear();
 
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
+        for (auto& s : shapes)
+        {
+            if (s->shape->getPosition().x < 0 ||
+                s->shape->getPosition().x + s->shape->getLocalBounds().width > window_w)
+            {
+                s->speed.x *= -1;
+            }
+            if (s->shape->getPosition().y < 0 ||
+                s->shape->getPosition().y + s->shape->getLocalBounds().height > window_h)
+            {
+                s->speed.y *= -1;
+            }
 
-	return 0;
+            s->draw(&window);
+            s->move();
+        }
+        window.display();
+    }
+
+    return 0;
 }
