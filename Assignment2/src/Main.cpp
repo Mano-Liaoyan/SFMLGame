f508f125a0d8c433fc1d4c7c3c5b19aeb6eec5d5
Yanyifan Liao <ben.jaminlyyf21@gmail.com>
2024-07-13 05:29:08 +0200

    Add initial setup to Assignment 2

--- Assignment2/src/Main.cpp
+++ Assignment2/src/Main.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello" << "\n";
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML Test");
+	// set this to avoid speed too fast
+	// window.setFramerateLimit(60);
+	window.setVerticalSyncEnabled(true);
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.display();
+	}
 	return 0;
 }
